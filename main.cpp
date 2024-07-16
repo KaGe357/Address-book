62ee64f56f9247ed83e2e2fa334c4e3615906fae
Kamil Gedaj <kamil.gedaj.programista@gmail.com>
2024-07-16 14:51:08 +0200

    main

---
+++ main.cpp
@@ -0,0 +1,514 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <windows.h>
+#include <conio.h>
+#include <vector>
+#include <cstdio>
+
+int numOfContacts;
+
+using namespace std;
+
+struct Contact
+{
+    int id;
+    string name, lastName, phoneNum, email, homeAdress;
+};
+
+void addNewContact(vector<Contact> &contacts);
+void loadFile(vector<Contact> &contacts);
+void printAllContacts(vector<Contact> contacts);
+void searchByName(vector<Contact> contacts);
+void application();
+void searchMenu();
+void searchByLastName(vector<Contact> contacts, string lastNameToFind);
+void searchByName(vector<Contact> contacts, string nameToFind);
+void removeContact(vector<Contact> &contacts, int idToRemove);
+void updateFile(vector<Contact> &contacts);
+void modifyContact(vector<Contact> &contacts, int idToModify);
+int checkHowManyLinesInFile(vector<Contact> contacts);
+int printMenuAndReturnOption();
+
+int main()
+{
+    application();
+}
+
+
+void modifyContact(vector<Contact> &contacts, int idToModify)
+{
+    int i = 0;
+    string temp = "";
+    while (contacts[i].id != idToModify) i++;
+        cout << "Id:                     " << contacts[i].id << endl;
+        cout << "Imie:                   " << contacts[i].name << endl;
+        cout << "Nazwisko:               " << contacts[i].lastName << endl;
+        cout << "Numer telefonu:         " << contacts[i].phoneNum << endl;
+        cout << "Email:                  " << contacts[i].email << endl;
+        cout << "Adres:                  " << contacts[i].homeAdress << endl << endl;
+
+    char option;
+    cout << "Wybierz dane do edycji." << endl;
+    cout << "1. Imie" << endl;
+    cout << "2. Nazwisko." << endl;
+    cout << "3. Numer telefonu." << endl;
+    cout << "4. Adres e-mail." << endl;
+    cout << "5. Adres domowy." << endl;
+    cout << "9. Powrot do menu glownego"<<endl;
+    cin >> option;
+
+    switch (option)
+    {
+    case '1':
+        cout << "Podaj nowe imie: ";
+        cin >> temp;
+        contacts[i].name = temp;
+        break;
+    case '2':
+        cout << "Podaj nowe nazwisko: ";
+        cin >> temp;
+        contacts[i].lastName = temp;
+        break;
+    case '3':
+        cout << "Podaj nowy numer telefonu: ";
+        cin >> temp;
+        contacts[i].phoneNum = temp;
+        break;
+    case '4':
+        cout << "Podaj nowy email: ";
+        cin >> temp;
+        contacts[i].email = temp;
+        break;
+    case '5':
+        cout << "Podaj nowy adres: ";
+        cin >> temp;
+        contacts[i].homeAdress = temp;
+        break;
+    case '0':
+        Sleep(500);
+        application();
+        break;
+    }
+
+    cout << "Kontakt zmieniony" << endl;
+    system("cls");
+            cout << "Id:                     " << contacts[i].id << endl;
+        cout << "Imie:                   " << contacts[i].name << endl;
+        cout << "Nazwisko:               " << contacts[i].lastName << endl;
+        cout << "Numer telefonu:         " << contacts[i].phoneNum << endl;
+        cout << "Email:                  " << contacts[i].email << endl;
+        cout << "Adres:                  " << contacts[i].homeAdress << endl << endl;
+        system("pause");
+
+    updateFile(contacts);
+}
+
+void removeContact(vector<Contact> &contacts, int idToRemove)
+{
+    int i = 0;
+    while (contacts[i].id != idToRemove)
+    {
+        i++;
+    }
+
+    contacts.erase(contacts.begin() + i);
+    updateFile(contacts);
+}
+
+void updateFile(vector<Contact> &contacts)
+{
+    fstream file;
+    file.open("kontakty.txt", ios::out);
+    if (file.good() == true)
+    {
+        for (const auto &contact : contacts)
+        {
+            file << contact.id << "|"
+                 << contact.name << "|"
+                 << contact.lastName << "|"
+                 << contact.phoneNum << "|"
+                 << contact.email << "|"
+                 << contact.homeAdress << "|" << endl;
+        }
+    }
+    else
+    {
+        cout << "Nie mozna uzyskac dostepu do pliku.";
+    }
+    file.close();
+}
+
+void searchMenu()
+{
+    char option;
+
+    cout << "0. Powrot do poprzedniego menu" << endl;
+    option = getch();
+    if (option == '0')
+    {
+        Sleep(500);
+        application();
+    }
+    else
+    {
+        cout << "Nieprawidlowy wybor.";
+        Sleep(2000);
+        searchMenu();
+    }
+}
+
+void searchByLastName(vector<Contact> contacts, string lastNameToFind)
+{
+    for (size_t i = 0; i < contacts.size(); i++)
+    {
+        if (contacts[i].lastName == lastNameToFind)
+        {
+            cout << "Id:                     " << contacts[i].id << endl;
+            cout << "Imie:                   " << contacts[i].name << endl;
+            cout << "Nazwisko:               " << contacts[i].lastName << endl;
+            cout << "Numer telefonu:         " << contacts[i].phoneNum << endl;
+            cout << "Email:                  " << contacts[i].email << endl;
+            cout << "Adres:                  " << contacts[i].homeAdress << endl
+                 << endl;
+        }
+    }
+}
+
+void searchByName(vector<Contact> contacts, string nameToFind)
+{
+    for (size_t i = 0; i < contacts.size(); i++)
+    {
+        if (contacts[i].name == nameToFind)
+        {
+            cout << "Id:                     " << contacts[i].id << endl;
+            cout << "Imie:                   " << contacts[i].name << endl;
+            cout << "Nazwisko:               " << contacts[i].lastName << endl;
+            cout << "Numer telefonu:         " << contacts[i].phoneNum << endl;
+            cout << "Email:                  " << contacts[i].email << endl;
+            cout << "Adres:                  " << contacts[i].homeAdress << endl
+                 << endl;
+        }
+    }
+}
+
+void application()
+{
+    fstream file;
+    Contact contact;
+    vector<Contact> contacts;
+loadFile(contacts);
+    string nameToFind, lastNameToFind;
+
+    while (true)
+    {
+        system("cls");
+        cout << " ________________________________" << endl;
+        cout << "|" << "    >>> KSIAZKA ADRESOWA <<<"
+             << "    |" << endl;
+        cout << "|" << " 1. Dodaj nowa osobe."
+             << "           |" << endl;
+        cout << "| 2. Wyszukaj po imieniu."
+             << "        |" << endl;
+        cout << "| 3. Wyszukaj po nazwisku."
+             << "       |" << endl;
+        cout << "| 4. Wyswietl wszystkie kontakty."
+             << "|"
+             << endl;
+        cout << "| 5. Usun adresata.              |" << endl;
+        cout << "| 6. Edytuj adresata.            |" << endl;
+        cout << "| 9. Zamknij program."
+             << "            |" << endl;
+        cout << "|________________________________|" << endl;
+        char option;
+        cin >> option;
+
+        if (option == '1')
+        {
+            cin.sync();
+            addNewContact(contacts);
+
+
+            Sleep(1000);
+            system("cls");
+            cout << "Kontakt dodany.";
+            Sleep(2000);
+            application();
+        }
+
+        if (option == '2')
+        {
+            system("cls");
+            cout << "Wpisz szukane imie: ";
+            cin >> nameToFind;
+            cout << endl;
+            searchByName(contacts, nameToFind);
+            searchMenu();
+        }
+
+        if (option == '3')
+        {
+            system("cls");
+            cout << "Wpisz szukane nazwisko: ";
+            cin >> lastNameToFind;
+            cout << endl;
+            searchByLastName(contacts, lastNameToFind);
+            searchMenu();
+        }
+
+        if (option == '4')
+        {
+            system("cls");
+            printAllContacts(contacts);
+            cout << "Powrot do poprzedniego menu - wcisnij 0";
+
+            option = getch();
+            if (option == '0')
+            {
+                Sleep(500);
+                application();
+            }
+        }
+
+        if (option == '5')
+        {
+            system("cls");
+            int idToRemove;
+            int i=0;
+            cout << "Podaj ID do usuniecia: ";
+            cin >> idToRemove;
+            while(contacts[i].id != idToRemove) i++;
+
+        cout << "Id:                     " << contacts[i].id << endl;
+        cout << "Imie:                   " << contacts[i].name << endl;
+        cout << "Nazwisko:               " << contacts[i].lastName << endl;
+        cout << "Numer telefonu:         " << contacts[i].phoneNum << endl;
+        cout << "Email:                  " << contacts[i].email << endl;
+        cout << "Adres:                  " << contacts[i].homeAdress << endl << endl;
+            cout << "Czy usunac kontakt? Y/N" << endl;
+            char option;
+            i=0;
+            cin >> option;
+            if(option=='y' || option=='Y') removeContact(contacts, idToRemove);
+
+            cout << "Kontakt usuniety" << endl;
+            Sleep(1000);
+            system("cls");
+            printAllContacts(contacts);
+            cout << "Powrot do poprzedniego menu - wcisnij 0";
+
+            option = getch();
+            if (option == '0')
+            {
+                Sleep(500);
+                application();
+            }
+        }
+
+        if (option == '6')
+        {
+            system("cls");
+            int idToModify;
+            cout << "Podaj ID do edycji: ";
+            cin >> idToModify;
+            modifyContact(contacts, idToModify);
+            Sleep(1000);
+            system("cls");
+            printAllContacts(contacts);
+            cout << "Powrot do poprzedniego menu - wcisnij 0";
+
+            option = getch();
+            if (option == '0')
+            {
+                Sleep(500);
+                application();
+            }
+        }
+
+        if (option == '9')
+        {
+            exit(0);
+        }
+    }
+
+    file.close();
+}
+
+void addNewContact(vector<Contact> &contacts)
+{
+    cin.sync();
+
+    fstream file;
+    string name, lastName, phoneNum, email, homeAdress;
+    string line;
+    string option = "";
+    Contact contact;
+    int position = 0;
+    file.open("kontakty.txt", ios::out | ios::app);
+    if (file.good() == true)
+    {
+        contact.id = contacts.back().id + 1;
+        file << contact.id << "|";
+        cout << "Podaj imie: ";
+        getline(cin, name);
+        contact.name = name;
+        file << name << "|";
+        cout << "Podaj nazwisko: ";
+        getline(cin, lastName);
+        contact.lastName = lastName;
+        file << lastName << "|";
+        cout << "Podaj numer telefonu: ";
+        getline(cin, phoneNum);
+        contact.phoneNum = phoneNum;
+        file << phoneNum << "|";
+        cout << "Podaj adres email: ";
+        getline(cin, email);
+        contact.email = email;
+        file << email << "|";
+        cout << "Podaj adres domowy: ";
+        getline(cin, homeAdress);
+        contact.homeAdress = homeAdress;
+        file << homeAdress << "|" << endl;
+        file.close();
+        position++;
+        contacts.push_back(contact);
+        Sleep(1000);
+            system("cls");
+            cout << "Kontakt dodany.";
+            Sleep(2000);
+            application();
+        /*  SPRAWDZANIE CZY KONTAKT ISTNIEJE I EWENTUALNA AKTUALIZACJA - TODO
+       */
+    }
+}
+
+void loadFile(vector<Contact> &contacts)
+{
+    fstream file;
+    file.open("kontakty.txt", ios::in);
+    Contact contact;
+    string line = "";
+    string temp;
+    int numberOfLine = 1;
+    char separator = '|';
+    int repsOfSeparator = 0;
+    if (file.good())
+    {
+        while (getline(file, line))
+        {
+            for (size_t i = 0; i < line.length(); i++)
+            {
+                if (line[i] != separator)
+                {
+                    while (repsOfSeparator == 0 && isdigit(line[i]) && line[i] != separator)
+                    {
+                        /* ZCZYTAJ ID*/
+                        temp += line[i];
+
+                        i++;
+                        if (line[i] == separator)
+                        {
+                            int id = stoi(temp);
+                            contact.id = id;
+                            repsOfSeparator++;
+                            i++;
+                        }
+                    }
+
+                    temp = "";
+                    while (repsOfSeparator == 1 && line[i] != separator)
+                    {
+                        /* ZCZYTAJ IMIE*/
+                        temp += line[i];
+
+                        i++;
+                        if (line[i] == separator)
+                        {
+                            contact.name = temp;
+                            repsOfSeparator++;
+                            i++;
+                        }
+                    }
+                    temp = "";
+                    while (repsOfSeparator == 2 && line[i] != separator)
+                    {
+                        /* ZCZYTAJ NAZWISKO*/
+                        temp += line[i];
+
+                        i++;
+                        if (line[i] == separator)
+                        {
+                            contact.lastName = temp;
+                            repsOfSeparator++;
+                            i++;
+                        }
+                    }
+                    temp = "";
+                    while (repsOfSeparator == 3 && line[i] != separator)
+                    {
+                        /* ZCZYTAJ NR TELEFONU*/
+                        temp += line[i];
+
+                        i++;
+                        if (line[i] == separator)
+                        {
+                            contact.phoneNum = temp;
+                            repsOfSeparator++;
+                            i++;
+                        }
+                    }
+                    temp = "";
+                    while (repsOfSeparator == 4 && line[i] != separator)
+                    {
+                        /* ZCZYTAJ EMAIL*/
+                        temp += line[i];
+
+                        i++;
+                        if (line[i] == separator)
+                        {
+                            contact.email = temp;
+                            repsOfSeparator++;
+                            i++;
+                        }
+                    }
+                    temp = "";
+                    while (repsOfSeparator == 5 && line[i] != separator)
+                    {
+                        /* ZCZYTAJ ADRES*/
+                        temp += line[i];
+
+                        i++;
+                        if (line[i] == separator)
+                        {
+                            contact.homeAdress = temp;
+                            repsOfSeparator++;
+                            i++;
+                        }
+                    }
+                    temp = "";
+                    contacts.push_back(contact);
+                }
+            }
+            numberOfLine++;
+            repsOfSeparator = 0;
+        }
+    }
+
+    file.close();
+
+}
+
+void printAllContacts(vector<Contact> contacts)
+{
+    int numOfContacts = contacts.size();
+    for (int i = 0; i < numOfContacts; i++)
+    {
+        cout << "Id:                     " << contacts[i].id << endl;
+        cout << "Imie:                   " << contacts[i].name << endl;
+        cout << "Nazwisko:               " << contacts[i].lastName << endl;
+        cout << "Numer telefonu:         " << contacts[i].phoneNum << endl;
+        cout << "Email:                  " << contacts[i].email << endl;
+        cout << "Adres:                  " << contacts[i].homeAdress << endl << endl;
+    }
+}
+
